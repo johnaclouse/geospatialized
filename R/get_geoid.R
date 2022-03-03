3c8393222d5ed341e21de4e1463760b0022505a1
John A Clouse <john@clouse.io>
2022-03-03 01:21:49 -0800

    Changed functions to use location data frame with lon and lat columns rather than independent vectors. Speed increase on get_geoid was orders of magnitude.

--- R/get_geoid.R
+++ R/get_geoid.R
@@ -1,32 +1,54 @@
 #' get_geoid
 #'
-#' @param lon numeric longitude of target location
-#' @param lat numeric latitude of target location
+#' @param location data frame with columns "lon" and "lat"
 #' @param acs_geometry list of acs geometries
 #'
-#' If the acs_geometry is at the block group level, the returned GEOID will be
-#' the block group of the target location. Likewise, if the acs_geometry is at
-#' the census tract level, the GEOID returned will be the census tract
-#' containing the target location.
+#'   If the acs_geometry is at the block group level, the returned GEOID will be
+#'   the block group of the target location. Likewise, if the acs_geometry is at
+#'   the census tract level, the GEOID returned will be the census tract
+#'   containing the target location.
 #'
-#' @return charcter value of GEOID (FIPS code)
+#'   When a location lies on the edge of multiple geographies, only the first
+#'   detected geography will be returned. If multiple geographies are detected,
+#'   the \code{multiple_intersections} flag is set to \code{TRUE}.
+#'
+#' @return data frame containing input coordinates, GEOID for geographic region,
+#'   geography name, multiple intersections flag, ACS total population, and
+#'   matching geometry
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' get_geoid(lon = GOTO_M1$lon, lat = GOTO_M1$lat)
+#' options(tigris_use_cache = TRUE)
+#' get_geoid(GOTO_M1)
 #' }
 
-get_geoid <- function(lon,
-                      lat,
-                      acs_geometry = get_acs_geometry()$block_groups
-) {
+get_geoid <- function(location,
+                      acs_geometry = get_acs_geometry()$block_groups) {
 
-  pt <- data.frame(lon, lat) %>%
-    sf::st_as_sf(coords = c("lon", "lat"),
-                 crs = sf::st_crs(acs_geometry))
-  # ensure only one row is returned
-  census_row = unlist(sf::st_within(pt, acs_geometry))[1]
 
-  return(acs_geometry[census_row, ]$GEOID)
+  locations <- sf::st_as_sf(location, coords = c("lon", "lat"),
+                            crs = sf::st_crs(acs_geometry))
+
+
+  geometry_row = ((sf::st_within(locations, acs_geometry)))
+
+  # sapply used to extract the first sub-element only
+  # prevents points on the edge of multiple polygons from producing multiple matches
+
+  multiple_instersections <- lengths(geometry_row) > 1
+
+  geometry_information <- acs_geometry[sapply(geometry_row, "[[", 1), ]
+  geometry_information <- cbind(lon = location$lon,
+                                lat = location$lat,
+                                geometry_information,
+                                multiple_instersections)
+  input_count <- nrow(location)
+  output_count <- nrow(geometry_information)
+
+  if (input_count != output_count)
+    stop(glue::glue("get_geoid received {input_count} rows but returned {output_count} rows."))
+
+  return(geometry_information)
 }
+
--- R/plot_geographic_areas.R
+++ R/plot_geographic_areas.R
@@ -2,8 +2,7 @@
 #'
 #' Plot the state, county, census tract, and block group geographic areas containing the target_location.
 #'
-#' @param lon numeric longitude of target
-#' @param lat numeric latitude of target
+#' @param location data frame with columns "lon" and "lat"
 #' @param states_needed character vector containing states needed for geography geometeries
 #' @param acs_geometry a list of acs_geometries from \code{get_acs_geometry()}
 #'
@@ -11,8 +10,7 @@
 #' @export
 #'
 #' @examples
-plot_geographic_areas <- function(lon = geospatialized::GOTO_M1$lon,
-                                  lat = geospatialized::GOTO_M1$lat,
+plot_geographic_areas <- function(location = geospatialized::GOTO_M1,
                                   states_needed = "Oregon",
                                   acs_geometry = NULL) {
 
@@ -22,13 +20,13 @@ plot_geographic_areas <- function(lon = geospatialized::GOTO_M1$lon,
     acs_geometry <- get_acs_geometry(states_needed)
 
 
-  target_block_group <- get_geoid(lon, lat, acs_geometry$block_groups)
+  target_block_group <- get_geoid(location, acs_geometry$block_groups)$GEOID
   target_census_tract <- stringr::str_sub(as.character(target_block_group), 1, 11)
   target_county <- stringr::str_sub(as.character(target_block_group), 1, 5)
   target_state <- stringr::str_sub(as.character(target_block_group), 1, 2)
 
   target_sf <-
-    sf::st_as_sf(data.frame(lon,lat),
+    sf::st_as_sf(location,
                  coords = c("lon", "lat"), crs = 4326, agr = "constant")
 
   leaflet::leaflet() %>%
@@ -37,36 +35,36 @@ plot_geographic_areas <- function(lon = geospatialized::GOTO_M1$lon,
       data =   dplyr::filter(acs_geometry$states, stringr::str_sub(GEOID, 1, 2) == target_state),
       popup = ~ paste(GEOID,  NAME),
       smoothFactor = 0,
-      weight = 4,
+      weight = 1,
       fillOpacity = 0,
-      color = "#60a3d9") %>%
+      color = "#000000") %>%
     leaflet::addPolygons(
       data =   dplyr::filter(acs_geometry$counties, stringr::str_sub(GEOID, 1, 5) == target_county),
-      popup = ~ paste(GEOID,  NAME),
-      smoothFactor = 0,
-      weight = 3,
-      fillOpacity = 0,
-      color = "#0074B7") %>%
-    leaflet::addPolygons(
-      data =   dplyr::filter(acs_geometry$census_tracts, stringr::str_sub(GEOID, 1, 11) == target_census_tract),
       popup = ~ paste(GEOID,  NAME),
       smoothFactor = 0,
       weight = 2,
       fillOpacity = 0,
       color = "#003B73") %>%
     leaflet::addPolygons(
-      data =   dplyr::filter(acs_geometry$block_groups,GEOID == target_block_group),
+      data =   dplyr::filter(acs_geometry$census_tracts, stringr::str_sub(GEOID, 1, 11) == target_census_tract),
       popup = ~ paste(GEOID,  NAME),
       smoothFactor = 0,
       weight = 2,
+      fillOpacity = 0,
+      color = "#0074B7") %>%
+    leaflet::addPolygons(
+      data =   dplyr::filter(acs_geometry$block_groups,GEOID == target_block_group),
+      popup = ~ paste(GEOID,  NAME),
+      smoothFactor = 0,
+      weight = 4,
       fillOpacity =0.1,
-      color = "#000000",
-      fillColor = "#000000") %>%
+      color = "#60a3d9",
+      fillColor = "#60a3d9") %>%
     leaflet::addCircleMarkers(data = target_sf,
-                     weight = 2,
+                     weight = 3,
                      fillOpacity = 0,
                      color = "#000000",
-                     popup = paste(lon, lat, sep = ", ")) %>%
-    leaflet::setView(lon, lat, zoom = 12)
+                     popup = paste(location$lon, location$lat, sep = ", ")) %>%
+    leaflet::setView(location$lon, location$lat, zoom = 12)
 }
 
